312ef762c226ce7c7e98c7af634dec090e9a8645
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-15 14:11:52 -0400

    initial commit

---
+++ exercises/1-12-1.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function displayMailingLabel(name, address, city, zip){
+console.log(name, address, city, zip);
+}
+displayMailingLabel("Jason Funderberker", "123 Street", "Garden Wall", "CT", "06107");
+
+function addNumbers(number1, number2){
+console.log(number1 + "+" + number2, "=" + (number1 + number2))
+}
+
+addNumbers(1,5)
