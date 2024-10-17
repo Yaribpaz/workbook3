95dcccc3834ea699a239a53a321d332896bc0ffe
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-17 14:33:59 -0400

    initial commit

---
+++ car-rental/main.js
@@ -0,0 +1,5 @@
+"use strict"
+
+let electronicRadio = document.getElementById("electronicRadio");
+let gpsRadio = document.getElementById("gpsRadio")
+let roadsideRadio = document.getElementById("roadsideRadio");
