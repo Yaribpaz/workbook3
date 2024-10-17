082186755ef47b3da3f9df108afe3799a8fb5d2c
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-17 10:20:58 -0400

    initial commit

---
+++ 2-15.js
@@ -0,0 +1,12 @@
+"use strict"
+let nameOfPerson = "Brenda Kaye";
+
+let positionOfSpace = nameOfPerson.indexOf(" ")
+console.log("Index: " + positionOfSpace)
+
+let firstname = nameOfPerson.substring(0,6)
+console.log("First Name: " + firstname)
+
+let lastname = nameOfPerson.substring(7,11)
+console.log("Last Name: " +lastname)
+
