be8d2282b8f872a611728516ca71de732eac2004
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-21 09:19:26 -0400

    math

--- car-rental/main.js
+++ car-rental/main.js
@@ -1,5 +1,15 @@
-"use strict"
+"use strict";
 
+console.log("got here");
+
+let numberOfDaysInput = document.getElementById("numberOfDaysInput");
 let electronicRadio = document.getElementById("electronicRadio");
-let gpsRadio = document.getElementById("gpsRadio")
+let gpsRadio = document.getElementById("gpsRadio");
 let roadsideRadio = document.getElementById("roadsideRadio");
+
+function estimateTotal() {
+  let numberOfDays = numberOfDaysInput.value;
+  let carRentalAmount = 29.99 * numberOfDays;
+
+  console.log(carRentalAmount);
+}
